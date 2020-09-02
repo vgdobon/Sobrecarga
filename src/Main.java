fc187d25cc26d8d826192446ec6b40e1295256fe
vgdobon <vgdobon91@gmail.com>
2020-09-02 10:02:16 +0200

    Ejemplo sobrecarga de métodos.

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Sobrecarga ejemplo = new Sobrecarga();
+
+        System.out.println(ejemplo.suma(1,2));
+        System.out.println(ejemplo.suma(1,2,3));
+    }
+}
---
+++ src/Sobrecarga.java
@@ -0,0 +1,10 @@
+public class Sobrecarga {
+
+    public int suma(int a, int b){
+        return a + b;
+    }
+
+    public int suma(int a, int b,int c){
+        return a + b + c;
+    }
+}
